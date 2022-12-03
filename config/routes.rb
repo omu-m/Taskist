c9f06a3e7ace580a01a491e1233a52284f5d9226
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-12-03 08:43:30 +0000

    [controller]追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'members/show'
-    get 'members/edit'
-    get 'members/unsubscribe'
-  end
-  namespace :public do
-    get 'targets/new'
-    get 'targets/index'
-    get 'targets/show'
-    get 'targets/edit'
-  end
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+  root to: "homes#top"
+end
+
 
 # 顧客用
 # URL /members/sign_in ...
@@ -23,5 +17,19 @@ devise_for :members, controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+scope module: :public do
+  root to: "homes#top"
+  get "/about" => "homes#about", as: "about"
+  get "/members/mypage" => "members#show", as: "mypage"
+  # members/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+  get "/members/information/edit" => "members#edit", as: "edit_information"
+  patch "/members/information" => "members#update", as: "update_information"
+  # 退会機能
+  get "/members/unsubscribe" => "members#unsubscribe", as: "confirm_unsubscribe"
+  put "/members/information" => "members#update"
+  patch "/members/withdrawal" => "members#withdrawal", as: "withdrawal_member"
+  resources :targets, only: [:index, :show, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
