7bad6ed67bc0a33e406628d448f8980e862beeaa
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-12-04 13:06:50 +0000

    [sign]新規登録作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -38,12 +39,21 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  def after_sign_up_path_for(resource)
+    mypage_path
+  end
+
+  def after_update_path_for(resource)
+    mypage_path
+  end
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :profile_image])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
