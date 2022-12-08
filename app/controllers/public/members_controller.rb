51b1942b7af75969d36e8efa5e6bfb8e4fdddbc1
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-12-08 12:39:12 +0000

    [mypage]作成

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,10 +1,19 @@
 class Public::MembersController < ApplicationController
+  before_action :authenticate_member!
+
   def show
+    @member = current_member
   end
 
   def edit
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:email, :name, :profile_image)
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,18 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 12 }
+  validates :email, presence: true, uniqueness: true
+
+  has_one_attached :profile_image
+
+  # プロフィール画像表示
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+    profile_image
+  end
 end
