e6f92d92c4ee395ffe95e8cd0e29279fc673355d
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-12-12 11:29:06 +0000

    [mypage]退会確認:作成

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -23,6 +23,15 @@ class Public::MembersController < ApplicationController
   def unsubscribe
   end
 
+  def withdrawal
+    @member = current_member
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @member.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました。"
+    redirect_to root_path
+  end
+
   private
 
   def member_params
