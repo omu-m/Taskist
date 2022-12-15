4930f6a512ca98859dba98c4c69c38adb7c8d672
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-12-15 13:12:30 +0000

    [target]目標新規作成:作成中

--- app/controllers/public/targets_controller.rb
+++ app/controllers/public/targets_controller.rb
@@ -1,5 +1,19 @@
 class Public::TargetsController < ApplicationController
+
   def new
+    @target = Target.new
+  end
+
+  def create
+    @target = Target.new(target_params)
+    @target.member_id = current_member.id
+    if @target.save
+      flash[:notice] = "正常に作成されました。"
+      redirect_to targets_path
+    else
+      flash[:alert] = "作成に失敗しました。"
+      render "new"
+    end
   end
 
   def index
@@ -9,5 +23,11 @@ class Public::TargetsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def target_params
+    params.require(:target).permit(:goal, :reason, :deadline)
   end
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,2 +1,6 @@
 class Target < ApplicationRecord
+
+  validates :goal, presence: true
+  validates :reason, presence: true
+  validates :deadline, presence: true
 end
