83a4fabcd1406126edbea9fe5a58a6501edc561b
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-01-09 06:39:57 +0000

    [admin]管理者ログイン作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, controllers: {
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
