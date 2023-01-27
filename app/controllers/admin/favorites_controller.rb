6694581164868f60a0f19d22e4f9ea882860b0ee
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-01-27 14:25:47 +0000

    [admin/favorites]リセット

--- app/controllers/admin/favorites_controller.rb
+++ app/controllers/admin/favorites_controller.rb
@@ -3,8 +3,5 @@ class Admin::FavoritesController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @member = current_member
-    @favorites = Favorite.where(member_id: @member).pluck(:target_id)
-    @targets = Target.order(created_at: :desc).find(@favorites)
   end
 end
